f26279a80cecf65c6792affeac19b93870b9cad0
Yiyeh <YiyehDev@gmail.com>
2023-03-04 16:34:58 -0300

    se agregó botón de resta y reset

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -5,19 +5,18 @@ export default function CounterApp( {value} ) {
 
     const [ counter, setCounter ] = useState ( value )
 
-    const handleAdd = (event, newValue) => {
-        setCounter ( counter + 1 );
-    };
+    const handleAdd = () => setCounter ( counter + 1 );
 
-    const handleSub = (event, newValue) => {
-        setCounter ( counter - 1 );
-    };
+    const handleSub = () => setCounter ( counter - 1 );
+
+    const handleReset = () => setCounter ( 0 );
 
     return (
         <>
             <h1>CounterApp</h1>
             <h2> { counter } </h2>
             <button onClick={handleAdd}> +1 </button>
+            <button onClick={handleReset}> Reset </button>
             <button onClick={handleSub}> -1 </button>
         </>
     )
